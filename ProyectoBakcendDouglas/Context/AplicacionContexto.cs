52373d377e9eec54d4694a609c4090678f4faffe
Douglas Marca <douglas.marca@uab.edu.bo>
2023-12-04 10:15:51 -0400

    Subiendo los proyectos base para el Frontend y Backend en la rama develop

---
+++ ProyectoBakcendDouglas/Context/AplicacionContexto.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ProyectoBackendDouglas.Context
+{
+    public class AplicacionContexto : DbContext
+    {
+        public AplicacionContexto
+            (DbContextOptions<AplicacionContexto> options)
+            : base(options) { }
+
+    }
+}
